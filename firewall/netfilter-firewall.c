81e2562c52ec204fa7a40dfe3f25f90a69badbb6
markmester <mmester6016@gmail.com>
2017-09-20 14:22:40 -0400

    removing bins

--- firewall/netfilter-firewall.c
+++ firewall/netfilter-firewall.c
@@ -13,19 +13,22 @@
 #include <linux/netfilter.h> 
 #include <linux/netfilter_ipv4.h> 
 #include <linux/net.h>
-#include <linux/in.h>
-#include <linux/skbuff.h>
 #include <linux/ip.h>
+#include <linux/skbuff.h>
 #include <linux/tcp.h>
+#include <linux/udp.h>
 
 /* ===============================================================================================
  * globals
  * * ===============================================================================================*/
-static unsigned char *blocked_ip = "\xD0\x50\x9A\xE0"; // IP we're blocking traffic 
+static char *blocked_ip = "208.80.154.240"; // IP we're blocking traffic 
 static char *blocked_interface = "lo"; // interface we're blocking traffic on
 struct sk_buff *sock_buff; // struct to copy packet over to
 struct udphdr *udp_header; // struct to copy udp header over to
 struct iphdr *ip_header;   // struct to copy ip header over to
+struct tcphdr *tcp_header;  // struct to copy tcp header over to
+unsigned int sport, dport;
+char source_addr[16], dest_addr[16];
 
 /* ===============================================================================================
  * module functions
@@ -36,27 +39,54 @@ unsigned int hook_func(
     const struct nf_hook_state *state
     ) 
 {
-    // block traffic recieved on "blocked_interface"
+    
+    // Drop packets recieved on lo interface
     if(strcmp((char*)state->in, blocked_interface) == 0) {
-    printk(KERN_INFO "Dropping packet recieved on interface: %s\n", state->in); 
+        printk(KERN_INFO "Dropping packet recieved on interface: %s\n", (char *)state->in); 
 
-    return NF_DROP;
+        // return NF_DROP;
+        return NF_ACCEPT;
     }
 
     // copy packet and grab network header
     sock_buff = skb;
-    ip_header = (struct iphdr*)skb_network_header(sock_buff);
     
-    // check for valid sk_buff and validate IP packet
-    //if(!sock_buff || !(sock_buff->nh.iph)) { return NF_ACCEPT; }    
+    //ip_header = (struct iphdr *)skb_network_header(sock_buff);
+    ip_header = ip_hdr(sock_buff);
 
-    // compare source address with block IP
-    //if(sock_buff->nh.iph->saddr == *(unsigned int*)blocked_ip) {
-    //    printk(KERN_INFO "Dropping packet with source address: %s\n", *(unsigned int*)blocked_ip);    
+    // check for valid sk_buff and validate IP packet
+    if(!sock_buff || !ip_header) { return NF_ACCEPT; }    
+
+    // TCP packet handling
+    if(ip_header->protocol == IPPROTO_TCP) { 
+        // first log some routing info
+        tcp_header = tcp_hdr(sock_buff);
+        snprintf(source_addr, 16, "%pI4", &ip_header->saddr);
+        snprintf(dest_addr, 16, "%pI4", &ip_header->daddr);
+        sport = htons((unsigned short int) tcp_header->source);
+        dport = htons((unsigned short int) tcp_header->dest);
+        printk(KERN_INFO "TCP route: %s:%d -> %s:%d\n", source_addr, sport, dest_addr, dport);
+        printk(KERN_INFO "SKBuffer: len %d, data_len %d\n", sock_buff->len, sock_buff->data_len);
+        
+        // now drop any packets recived from 208.80.154.224 (wikipedia)
+        if(strcmp(source_addr, blocked_ip) == 0) {
+            printk(KERN_INFO "Dropping packet recieved from Wikipedia\n");
             
-    //    return NF_DROP; 
+            return NF_DROP;
+        }
+        
+        return NF_ACCEPT; 
+    }
+
+    // drop icmp packets
+    if(ip_header->protocol == IPPROTO_ICMP) {
+        printk(KERN_INFO "Dropping packet with ICMP protocol\n");
+        
+        return NF_DROP;
+    }
+
+
     return NF_ACCEPT;
-    //}
 }
 
 static struct nf_hook_ops nfho = { // struct holding set of hook function options
