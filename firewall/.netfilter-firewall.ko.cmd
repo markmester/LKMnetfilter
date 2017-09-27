2841c8b56c7c8849336a0ae9ee8c39e06807a2d9
markmester <mmester6016@gmail.com>
2017-09-27 10:12:56 -0400

    removing bins

--- firewall/.netfilter-firewall.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/mark/CLionProjects/LKMnetfilter/firewall/netfilter-firewall.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/mark/CLionProjects/LKMnetfilter/firewall/netfilter-firewall.ko /home/mark/CLionProjects/LKMnetfilter/firewall/netfilter-firewall.o /home/mark/CLionProjects/LKMnetfilter/firewall/netfilter-firewall.mod.o ;  true
